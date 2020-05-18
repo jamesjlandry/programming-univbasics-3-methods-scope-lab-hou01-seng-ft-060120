75b534306bb658da4f5a72f0f051a1baaa354648
James Landry <james.j.landry@outlook.com>
2020-05-18 16:23:00 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -2,19 +2,19 @@ def mario
   phrase = "It's a me, Mario!"
   
   puts phrase
-  phrase
+  
 end
 
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
-  status
+  
 end
 
 def link
   words = "It's Dangerous To Go Alone! Take This."
   puts words
-  words
+  
 end
 
 def all_phrases
